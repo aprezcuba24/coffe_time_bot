5a22da505c8f76ac832b8103066cd0bdb84e6d1a
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-03-07 13:48:41 +0000

    Add debug prints.

--- app/schedule.py
+++ app/schedule.py
@@ -18,6 +18,7 @@ async def process_all(event):
     if not chat_data:
         return {"statusCode": 200, "body": "no_chat"}
     pivot_date = datetime.strptime(event["time"], "%Y-%m-%dT%H:%M:%SZ")
+    print("pivot_date =>", pivot_date)
     for chat_id, chat_item in chat_data.items():
         await process_chat(pivot_date=pivot_date, chat_id=chat_id, chat_data=chat_item)
     return {"statusCode": 200, "body": "Success"}
@@ -26,6 +27,7 @@ async def process_all(event):
 async def process_chat(pivot_date, chat_id, chat_data):
     chat_item = ChatItem(chat_id=chat_id, persistence=application.persistence)
     chat_item.load_data(chat_data=chat_data)
+    print("chat_data", chat_data)
     if chat_item.last_play_date:
         delta = pivot_date - chat_item.last_play_date
         pivot_hour = pivot_date.hour - HOURS_DIFFERENCE
@@ -39,11 +41,14 @@ async def process_chat(pivot_date, chat_id, chat_data):
 
 
 async def open_game_by_schedule(chat_id, chat_item: ChatItem):
+    print("open_game_by_schedule", chat_id)
     users = chat_item.open_game()
+    print("open_game_by_schedule users =>", users)
     if len(users) > 0:
         await chat_item.save()
         return await send_message(chat_id, users)
 
 
 async def send_message(chat_id, users):
+    print("send_message =>", chat_id)
     return await application.bot.send_message(chat_id=chat_id, **start_params(users))
